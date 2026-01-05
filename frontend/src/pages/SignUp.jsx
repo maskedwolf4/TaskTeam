f0f019211d742611aac73ae21e0e8d86383b1428
maskedwolf4 <meetwadekar7@gmail.com>
2026-01-05 20:07:54 +0530

    Addding Pages

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,9 @@
+import {SignUp} from "@clerk/clerk-react"
+
 function SignUpPage() {
-    return <></>
+    return <div className={"auth-container"}>
+        <SignUp routing={"path"} path={"/sign-up"} signInUrl={"/sign-in"}/>
+    </div>
 }
 
 export default SignUpPage
