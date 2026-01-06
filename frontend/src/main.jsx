b01bb467ae5e653df933773cf6ad6109ddb87a7f
maskedwolf4 <meetwadekar7@gmail.com>
2026-01-06 20:55:26 +0530

    Adding services

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,7 @@ import {BrowserRouter} from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
 
-// Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-    throw new Error('Add your Clerk Publishable Key to the .env file')
-  }
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
@@ -18,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
           <BrowserRouter>
               <App />
           </BrowserRouter>
